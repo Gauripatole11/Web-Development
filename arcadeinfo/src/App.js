462225bd4cc1325d55dde9ec232d97d3fbf5683f
Gauripatole11 <gauripatole9@gmail.com>
2023-01-28 22:51:48 -0800

    EXERCISE 1.4

--- arcadeinfo/src/App.js
+++ arcadeinfo/src/App.js
@@ -1,27 +1,22 @@
-
-// Exercise 1.3
-
-//Code
-
 const App = () => {
     // const-definitions
     const course = 'COMP 227 WEB DEVELOPMENT'
     const handhelds = [
         {
-            part0: 'Part 0',
-            exercise0: 11
+            part: 'Part 0',
+            exercise: 11
         },
         {
-            part1: 'Part 1',
-            exercise1: 9
+            part: 'Part 1',
+            exercise: 9
         }
     ]
 
     return (
         <div>
             <Header course={course} />
-            <Content part0={handhelds[0].part0} part1={handhelds[1].part1} part0exercise={handhelds[0].exercise0} part1exercise={handhelds[1].exercise1} />
-            <Total part0exercise={handhelds[0].exercise0} part1exercise={handhelds[1].exercise1} />
+            <Content handhelds={handhelds} />
+            <Total handhelds={handhelds} />
         </div>
     )
 }
@@ -30,8 +25,8 @@ const Content = (props) => {
 
     return (
         <div>
-            <Handheld part={props.part0} partexercise={props.part0exercise} />
-            <Handheld part={props.part1} partexercise={props.part1exercise} />
+            <Handheld part={props.handhelds[0].part} partexercise={props.handhelds[0].exercise} />
+            <Handheld part={props.handhelds[1].part} partexercise={props.handhelds[1].exercise} />
         </div>
     )
 }
@@ -40,7 +35,7 @@ const Total = (props) => {
 
     return (
         <div>
-            <p>There are total {props.part0exercise + props.part1exercise} exercises overall</p>
+            <p>There are total {props.handhelds[0].exercise + props.handhelds[1].exercise} exercises overall</p>
         </div>
     )
 }
